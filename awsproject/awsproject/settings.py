c9f462a357c4c7fc70d7a936f177ff1ce530c459
Santiago Sanchez Pulgarín <santiago.sanchez.ssaa@gmail.com>
2021-07-05 16:00:59 -0500

    feat: restore some settings configuration

--- awsproject/awsproject/settings.py
+++ awsproject/awsproject/settings.py
@@ -16,14 +16,14 @@ from pathlib import Path
 
 load_dotenv(find_dotenv(filename="../../.env"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*t%+-1&8!&5r$a56+5r4=^%ijp*sfb7=935z6@58k8j)l+_0k3'
+SECRET_KEY = os.getenv('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
