f88e8b430e1d727f177a17d7033157a8be41c76d
Santiago Sanchez Pulgarín <santiago.sanchez.ssaa@gmail.com>
2021-07-05 15:54:16 -0500

    feat: create app, set .env in settings and change requirements.txt

--- awsproject/awsproject/settings.py
+++ awsproject/awsproject/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
+load_dotenv(find_dotenv(filename="../../.env"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
